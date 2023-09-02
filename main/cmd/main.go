e788a78c78ea0d9a9abefc21ca4c644386abaf94
XenomorphsNightmare <gyarigeri12@gmail.com>
2023-09-02 11:51:47 +0200

    new go project

---
+++ main/cmd/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	ISBN     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["ID"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+
+		}
+
+	}
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "11032", ISBN: "202332:030", Title: "Bottle Rocket", Director: &Director{FirstName: "Wes", LastName: "Anderson"}})
+	movies = append(movies, Movie{ID: "11048", ISBN: "202342:030", Title: "Fitzcaraldo", Director: &Director{FirstName: "Werner", LastName: "Herzog"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovies).Methods("POST")
+	r.HandleFunc("/movies", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Starting server at 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+
+}
---
+++ pkg/routes/carvendor.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"/home/gergo/Documents/go_projects/crud/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterCarRoutes = func(router *mux.Router) {
+	router.HandleFunc("/car/", controllers.CreateCar).Methods("POST")
+	router.HandleFunc("/car/", controllers.GetCar).Methods("GET")
+	router.HandleFunc("/car/{carID}", controllers.GetCarByID).Methods("GET")
+	router.HandleFunc("car/{carID}", controllers.UpdateCar).Methods("PUT")
+	router.HandleFunc("car/{carID}", controllers.DeleteCar).Methods("DELETE")
+
+}
